08668ebec621c346e75b80ac3c28ca1b9ea79e37
mohammadbader99 <md.bader@hotmail.com>
2021-08-02 04:07:28 +0300

    Created HelloBot

---
+++ app.js
@@ -0,0 +1,17 @@
+alert("Welcome to our website..");
+
+let firstName = prompt('What is your frist name?');
+alert("Hello " + firstName);
+console.log("Users first name is " + firstName)
+
+let lastName = prompt('Last name?');
+alert("Hello " + firstName + " " + lastName);
+console.log("Users last name is " + lastName)
+
+let country = prompt('Where are you from?');
+alert("Hello " + firstName + " " + lastName + " from " + country);
+console.log("Users country is " + country)
+
+let movie = prompt('What is your favourite movie?');
+alert("Hello " + firstName + " " + lastName + " from " + country + ", so your favourite movie is " + movie + "? it is very nice.");
+console.log("Users favourite movie is " + movie)
